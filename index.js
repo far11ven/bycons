dc9b9d6bcf4e24f69f46bf7f75a688654aad4565
far11ven <kushalbhalaik@gmail.com>
2019-11-28 01:02:44 +0530

    v1.0.6

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const path = require("path");
 
 const iconPath = path.join(__dirname, "icons/");
+const finalPath = iconPath + "at.svg";
+const src = path.parse(finalPath);
+console.log("finalPath", finalPath);
 function get(svg) {
   if (typeof svg === "string" && svg.length > 0) {
-    const finalPath = iconPath + svg + ".svg";
-    return finalPath;
+    return src;
   }
-  
-  throw "requested icon doesn't exist"
+
+  throw "requested icon doesn't exist";
 }
 module.exports = get;
